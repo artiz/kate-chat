65f0b29e763c4b436ccaeb989846361ab0ba8f6c
artiz <artem.kustikov@gmail.com>
2025-07-15 16:35:04 +0200

    prettier

--- backend/src/services/bedrock/providers/anthropic.service.ts
+++ backend/src/services/bedrock/providers/anthropic.service.ts
@@ -39,13 +39,13 @@ type AnthropicRequestMessagePart =
   | string
   | { type: "text"; text: string }
   | {
-    type: "image";
-    source: {
-      type: "base64";
-      media_type: string;
-      data: string;
+      type: "image";
+      source: {
+        type: "base64";
+        media_type: string;
+        data: string;
+      };
     };
-  };
 
 export type AnthropicRequestMessage = {
   role: AnthropicMessageRole;
@@ -61,80 +61,92 @@ export class AnthropicService implements BedrockModelServiceProvider<AnthropicRe
 
     // Convert messages to Anthropic format
     // https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-anthropic-claude-messages.html
-    const anthropicMessages: AnthropicRequestMessage[] = messages.flatMap(msg => {
-      if (typeof msg.body === "string") {
-        return [{
-          role: msg.role === MessageRole.ASSISTANT ? "assistant" : "user",
-          content: msg.body,
-        } as AnthropicRequestMessage];
-      }
-
-      const images: AnthropicRequestMessagePart[] = msg.body.filter(m => m.contentType === "image").map(m => {
-        let base64Data = m.content;
-        let mediaType = m.mimeType || "image/jpeg";
-        // input format "data:image/jpeg;base64,{base64_image}"
-        const parts = m.content.match(/^data:(image\/[^;]+);base64,(.*)$/);
-        if (!parts && !mediaType) {
-          logger.error({ content: m.content.substring(0, 256) }, "Invalid image format");
-          throw new Error(
-            "Invalid image format, expected base64 data URL starting with 'data:image/xxxl;base64,...',"
-          );
-        } else if (parts) {
-          base64Data = parts[2]; // e.g., "iVBORw0KGgoAAAANSUhEUgAA..."
-          mediaType = parts[1]; // e.g., "jpeg", "png"
+    const anthropicMessages: AnthropicRequestMessage[] = messages
+      .flatMap(msg => {
+        if (typeof msg.body === "string") {
+          return [
+            {
+              role: msg.role === MessageRole.ASSISTANT ? "assistant" : "user",
+              content: msg.body,
+            } as AnthropicRequestMessage,
+          ];
         }
-        const image: AnthropicRequestMessagePart = {
-          type: "image",
-          source: {
-            type: "base64",
-            media_type: mediaType,
-            data: base64Data,
-          },
-        };
-        return image;
-      });
 
-      const texts: AnthropicRequestMessagePart[] = msg.body.filter(m => m.contentType === "text").map(m => {
-        return {
-          type: "text",
-          text: m.content,
-        };
-      });
+        const images: AnthropicRequestMessagePart[] = msg.body
+          .filter(m => m.contentType === "image")
+          .map(m => {
+            let base64Data = m.content;
+            let mediaType = m.mimeType || "image/jpeg";
+            // input format "data:image/jpeg;base64,{base64_image}"
+            const parts = m.content.match(/^data:(image\/[^;]+);base64,(.*)$/);
+            if (!parts && !mediaType) {
+              logger.error({ content: m.content.substring(0, 256) }, "Invalid image format");
+              throw new Error(
+                "Invalid image format, expected base64 data URL starting with 'data:image/xxxl;base64,...',"
+              );
+            } else if (parts) {
+              base64Data = parts[2]; // e.g., "iVBORw0KGgoAAAANSUhEUgAA..."
+              mediaType = parts[1]; // e.g., "jpeg", "png"
+            }
+            const image: AnthropicRequestMessagePart = {
+              type: "image",
+              source: {
+                type: "base64",
+                media_type: mediaType,
+                data: base64Data,
+              },
+            };
+            return image;
+          });
 
-      if (msg.role === MessageRole.USER) {
-        const userMessage: AnthropicRequestMessage = {
-          role: "user",
-          content: [...images, ...texts],
-        };
-        return [userMessage];
-      }
+        const texts: AnthropicRequestMessagePart[] = msg.body
+          .filter(m => m.contentType === "text")
+          .map(m => {
+            return {
+              type: "text",
+              text: m.content,
+            };
+          });
 
-      const result: AnthropicRequestMessage[] = [];
-      if (images.length) {
-        result.push({
-          role: "user",
-          content: [...images],
-        });
-      }
+        if (msg.role === MessageRole.USER) {
+          const userMessage: AnthropicRequestMessage = {
+            role: "user",
+            content: [...images, ...texts],
+          };
+          return [userMessage];
+        }
 
-      if (texts.length) {
-        result.push({
-          role: "assistant",
-          content: texts,
-        });
-      }
-      return result;
-    }).filter(notEmpty);
+        const result: AnthropicRequestMessage[] = [];
+        if (images.length) {
+          result.push({
+            role: "user",
+            content: [...images],
+          });
+        }
 
-    logger.debug({
-      modelId, messages: anthropicMessages.map(m => ({
-        role: m.role,
-        content: typeof (m.content) === "string" ? 
-          m.content.substring(0, 15) : 
-          m.content.map(c => typeof (c) === 'string' ? c.substring(0, 15) : ({ ...c, source: undefined })),
-      }))
-    }, "Call Anthropic model");
+        if (texts.length) {
+          result.push({
+            role: "assistant",
+            content: texts,
+          });
+        }
+        return result;
+      })
+      .filter(notEmpty);
 
+    logger.debug(
+      {
+        modelId,
+        messages: anthropicMessages.map(m => ({
+          role: m.role,
+          content:
+            typeof m.content === "string"
+              ? m.content.substring(0, 15)
+              : m.content.map(c => (typeof c === "string" ? c.substring(0, 15) : { ...c, source: undefined })),
+        })),
+      },
+      "Call Anthropic model"
+    );
 
     return {
       modelId,
