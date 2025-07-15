35b2252840c6ea7a08762b324845c3d0401db1d3
artiz <artem.kustikov@gmail.com>
2025-07-15 16:20:51 +0200

    update GraphqQL interfaces

--- backend-rust/migrations/2025-07-12-194526_add_user_role_and_settings/down.sql
+++ backend-rust/migrations/2025-07-12-194526_add_user_role_and_settings/down.sql
@@ -1,7 +1,7 @@
 alter table users drop column "role";
 alter table users drop column "settings";
 
-alter table messages drop column "jsonContent";
+alter table messages drop column "json_content";
 alter table messages drop column "metadata";
 
 
