8f7529532ceeb6e77fb6e22f1091daeff22f6b7c
artiz <artem.kustikov@gmail.com>
2025-09-08 09:05:14 +0200

    fix messages content column type update/revert for Postgres

--- db-migrations/postgres/1757012213090-tune_types.ts
+++ db-migrations/postgres/1757012213090-tune_types.ts
@@ -27,10 +27,20 @@ export class TuneTypes1757012213090 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "messages" ADD "content_tmp" text NOT NULL DEFAULT ''`,
+    );
+    await queryRunner.query(
+      `UPDATE "messages" SET "content_tmp" = COALESCE("content"::text, '')`,
+    );
+
     await queryRunner.query(`ALTER TABLE "messages" DROP COLUMN "content"`);
     await queryRunner.query(
       `ALTER TABLE "messages" ADD "content" character varying NOT NULL DEFAULT ''`,
     );
+    await queryRunner.query(`UPDATE "messages" SET "content" = "content_tmp"`);
+    await queryRunner.query(`ALTER TABLE "messages" DROP COLUMN "content_tmp"`);
+
     await queryRunner.query(
       `ALTER TABLE "models" ALTER COLUMN "description" SET NOT NULL`,
     );
