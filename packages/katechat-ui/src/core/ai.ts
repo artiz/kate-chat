cc2a8eb6c1ac101a8d0cf2d075e559d5dbb28bc2
artiz <artem.kustikov@gmail.com>
2026-02-09 12:08:29 +0100

    katechat-ui: formatting

--- packages/katechat-ui/src/core/ai.ts
+++ packages/katechat-ui/src/core/ai.ts
@@ -1,1 +1,8 @@
-export type ApiProvider = "AWS_BEDROCK" | "OPEN_AI" | "YANDEX_FM" | "GOOGLE_VERTEX_AI" | "DEEPSEEK" | "ANTHROPIC" | "CUSTOM_REST_API";
+export type ApiProvider =
+  | "AWS_BEDROCK"
+  | "OPEN_AI"
+  | "YANDEX_FM"
+  | "GOOGLE_VERTEX_AI"
+  | "DEEPSEEK"
+  | "ANTHROPIC"
+  | "CUSTOM_REST_API";
